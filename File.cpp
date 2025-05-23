754926c166e3569e4630cf5197d2c591e0e6753c
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-23 08:24:12 +0700

    Case 2, Stack is empty

--- File.cpp
+++ File.cpp
@@ -101,6 +101,17 @@ int main()
         cin >> value;
         stack.push(value); //Push the entered value onto the stack
         break;
+        case 2:
+        if (!stack.isEmpty())
+        {
+            stack.pop(); // Pop the top element from the stack
+        }
+        else
+        {
+            cout << "stack is empty. Cannot pop." << endl;
+        }
+        break;
+
     }
 
     }
