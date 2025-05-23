4b0030e89b9b0d4f5bbed6a9490b47aba771246f
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-23 07:57:41 +0700

    push operation

--- File.cpp
+++ File.cpp
@@ -26,5 +26,17 @@ class stack
         top = NULL; // Intialize the stack with a null top pointer
     }
 
+    //push operation: Isert an element onto the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. Allocate memory for the new mode
+        newNode->data = value; // 2. Assign value
+        newNode->next= top; // 3. Set the next pointer of the node to
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
+
    
+
 }
