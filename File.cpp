31c92d0f396b91f94be3c27e0d6cc2bd78b0791f
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-23 07:58:47 +0700

    Pop operation

--- File.cpp
+++ File.cpp
@@ -37,6 +37,13 @@ class stack
         return value;
     }
 
-   
+    //pop operation: remove the topmost element from the stack
+    void pop()
+    {
+        if(isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+    }
 
 }
