b18a10819f4490bfac3ec35faa84a289a6ac9bb9
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-23 08:02:03 +0700

    Cout and delete in pop operation

--- File.cpp
+++ File.cpp
@@ -47,6 +47,9 @@ class stack
 
         Node *temp = top; // Create a temporary pointer to the top node
         top = top->next; // Update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+        delete temp;
     }
+    
 
 }
